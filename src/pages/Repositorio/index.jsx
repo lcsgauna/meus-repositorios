497deb9d81c41b78bfa1c8e04b334a738e19e3ba
Lucas Gauna Samudio <lucasgsamudio@gmail.com>
2024-03-03 22:44:34 -0400

    feat: Adicionando carregamento de detalhes

--- src/pages/Repositorio/index.jsx
+++ src/pages/Repositorio/index.jsx
@@ -1,9 +1,39 @@
 import { useParams } from "react-router-dom"
+import { Container } from "./styles";
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
+
 export default function Repositorio(){
     const { repositorio } = useParams();
+    const [repo,setRepo] = useState({});
+    const [issues,setIssues] = useState([]);
+    const [loading,setLoading] = useState(true);
+
+
+    useEffect(() => {
+        async function load(){
+            const nomeRepo = decodeURI(repositorio);
+            const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${nomeRepo}`), 
+                api.get(`/repos/${nomeRepo}/issues`,{
+                    params : {
+                        state : 'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+            setRepo(repositorioData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
+        }
+
+        load();
+    },[repositorio]);
+    useState
     return(
-        <h1 style={{color:'#fff'}}>
-            {decodeURI(repositorio)}
-        </h1>
+       <Container>
+
+       </Container>
     )
 }

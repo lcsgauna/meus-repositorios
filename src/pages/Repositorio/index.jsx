093833b9f9e9043aea03768bef7c979688bb230b
Lucas Gauna Samudio <lucasgsamudio@gmail.com>
2024-03-03 23:57:04 -0400

    feat: Exibindo detalhes

--- src/pages/Repositorio/index.jsx
+++ src/pages/Repositorio/index.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
-import { Container } from "./styles";
+import { Container, Owner, Loading,BackButton } from "./styles";
 import { useEffect, useState } from "react";
+import { FaArrowLeft } from "react-icons/fa";
 import api from "../../services/api";
 
 
@@ -31,9 +32,27 @@ export default function Repositorio(){
         load();
     },[repositorio]);
     useState
+
+
+    if(loading){
+        return(<Loading><h1>Carregando...</h1></Loading>)
+    }
+
+
     return(
        <Container>
-
+        <BackButton to="/">
+           <FaArrowLeft color="#000"/> 
+        </BackButton>
+            <Owner>
+                <img src={repo.owner.avatar_url} alt={repo.owner.login}/>
+                <h1>
+                    {repo.name}
+                </h1>
+                <p>
+                    {repo.description}
+                </p>
+            </Owner>
        </Container>
     )
 }

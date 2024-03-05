623078051acdbba3003a9dc270cb079ba5090408
Lucas Gauna Samudio <lucasgsamudio@gmail.com>
2024-03-04 23:44:10 -0400

    feat: Criando paginacao

--- src/pages/Repositorio/index.jsx
+++ src/pages/Repositorio/index.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom"
-import { Container, Owner, Loading,BackButton } from "./styles";
+import { Container, Owner, Loading,BackButton, IssuesList } from "./styles";
 import { useEffect, useState } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import api from "../../services/api";
@@ -53,6 +53,23 @@ export default function Repositorio(){
                     {repo.description}
                 </p>
             </Owner>
+            <IssuesList>
+                {issues.map(issue => (
+                    <li key={String(issue.id)}>
+                        <img src={issue.user.avatar_url} alt={issue.user.login}/>
+                        <div>
+                            <strong>
+                                <a href={issue.html_url}>{issue.title}</a>
+
+                                {issue.labels.map((label) => (
+                                    <span key={String(label.id)}>{label.name}</span>
+                                ))}
+                            </strong>
+                            <p>{issue.user.login}</p>
+                        </div>
+                    </li>
+                ))}
+            </IssuesList>
        </Container>
     )
 }
